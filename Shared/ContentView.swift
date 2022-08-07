3f26685b812d7d25b47ea95613aea715ba12e486
Li-Huai (Allan) Lin <qqaatw@gmail.com>
2022-08-07 14:45:31 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Allan Lin on 2022/8/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MSCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSCalculatorApp.swift
+//  Shared
+//
+//  Created by Allan Lin on 2022/8/7.
+//
+
+import SwiftUI
+
+@main
+struct MSCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
